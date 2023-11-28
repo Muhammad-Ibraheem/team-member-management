6b65a9cc90562f3c4a004730fb84d453b030a712
ibraheem <ibrahimaziz719@gmail.com>
2023-11-27 23:20:07 -0500

    setup the project and define root route

---
+++ TeamMemberManagement/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    if request.method == "GET":
+        return render(request,'home.html')
